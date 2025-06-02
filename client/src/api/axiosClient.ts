9e51500bfdc709044dc655dee89e22e1c70a34ac
Summit Setia <summitsetia@gmail.com>
2025-06-02 22:22:05 +1200

    env file frontend

--- client/src/api/axiosClient.ts
+++ client/src/api/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "https://api.bank.summitsetia.com",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true 
 });
 
